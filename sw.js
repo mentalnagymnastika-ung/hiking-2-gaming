6782508c94fb70a631cc1d76644d7229a817f7c1
mentalnagymnastika-ung <mentalna.gymnastika@gmail.com>
2026-06-07 14:35:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 👇 NOVA SERIJA IGARA - KREĆEMO OD STOTKE 👇
-const CACHE_NAME = 'potraga-oflajn-v100.15'; 
+const CACHE_NAME = 'potraga-oflajn-v100.16'; 
 // 👆 ----------------------------------------------------------- 👆
 
 const OBAVEZNI_FAJLOVI = [
