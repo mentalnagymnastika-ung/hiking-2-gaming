c13e3861405af797a4e8c85cfd84df90ae51382d
mentalnagymnastika-ung <mentalna.gymnastika@gmail.com>
2026-06-09 08:22:12 +0200

    verzija  31.32

--- admin-sw.js
+++ admin-sw.js
@@ -1,5 +1,5 @@
 // Specifično ime keša za admina da se ne sudara sa običnim kreatorom
-const CACHE_NAME = 'admin-cache-v31.31';
+const CACHE_NAME = 'admin-cache-v31.32';
 
 // Spisak fajlova koje želimo odmah da sačuvamo za oflajn rad
 const urlsToCache = [
