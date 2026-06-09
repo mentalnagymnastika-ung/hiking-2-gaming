b7833f8231f612c54ba99c1f63f6cfb3d317e897
mentalnagymnastika-ung <mentalna.gymnastika@gmail.com>
2026-06-09 08:21:20 +0200

    Update service worker cache version to v100.20

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 👇 NOVA SERIJA IGARA - KREĆEMO OD STOTKE 👇
-const CACHE_NAME = 'potraga-oflajn-v100.19'; 
+const CACHE_NAME = 'potraga-oflajn-v100.20'; 
 // 👆 ----------------------------------------------------------- 👆
 
 const OBAVEZNI_FAJLOVI = [
