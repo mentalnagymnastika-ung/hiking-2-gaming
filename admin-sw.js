05da5429d74a158e48db03fc5ae332075cb0cbec
mentalnagymnastika-ung <mentalna.gymnastika@gmail.com>
2026-08-30 13:24:23 +0200

    Update cache name and version in admin service worker 33

--- admin-sw.js
+++ admin-sw.js
@@ -1,5 +1,5 @@
 // Specifično ime keša za admina da se ne sudara sa običnim kreatorom
-const CACHE_NAME = 'admin-cache-v31.36';
+const CACHE_NAME = 'admin-cache-v33.00';
 
 // Spisak fajlova koje želimo odmah da sačuvamo za oflajn rad
 const urlsToCache = [
